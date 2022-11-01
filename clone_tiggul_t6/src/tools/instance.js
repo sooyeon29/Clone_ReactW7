996a2a75c85c4f6632f003b7ef5a81877679d169
JeongWonji <Jungwonji1960@gmail.com>
2022-11-02 01:45:05 +0900

    ✨ [1102] 회원가입 기능 완성

--- clone_tiggul_t6/src/tools/instance.js
+++ clone_tiggul_t6/src/tools/instance.js
@@ -16,6 +16,7 @@ export const MyOttApi = {
   getOtt: (num) => instance.get(`/api/ott/${num}`),
   postBankAccount: (payload) => instance.post(`/mypage/account`),
   login: (payload) => instance.post(`/api/user/login`, payload),
+  signup: (payload) => instance.post(`/api/user`, payload),
 };
 
 // 로그인할때 헤더에 토큰 붙여서 주는 방식사용할때 쓰는 식
