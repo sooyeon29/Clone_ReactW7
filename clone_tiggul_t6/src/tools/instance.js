6d41cc6d05af0f99937c91e4e9eabdc00b66c9b3
JeongWonji <Jungwonji1960@gmail.com>
2022-11-02 18:16:49 +0900

    🔧 [1102] 머지 충돌 코드 수정

--- clone_tiggul_t6/src/tools/instance.js
+++ clone_tiggul_t6/src/tools/instance.js
@@ -1,15 +1,9 @@
 import axios from "axios";
-<<<<<<< HEAD
 
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOnsidXNlcklkIjoiMSJ9LCJpYXQiOjE2NjcyODgyODd9.enjRyabsUbMvONZb80h_YYdeNvXBZObabBqCyqCeEQg";
-
-=======
 // import { getCookie } from "../Components/estarlogin/cookiehook";
 const token = document.cookie.replace("Authorization=", "");
 // console.log(document.cookie);
 // console.log(token);
->>>>>>> feature/wj
 const instance = axios.create({
   // baseURL: "http://localhost:3001/",
   baseURL: "http://hi-prac.shop:3000/",
@@ -20,7 +14,6 @@ const instance = axios.create({
 
 export const MyOttApi = {
   getOtt: (num) => instance.get(`/api/ott/${num}`),
-  postBankAccount: (payload) => instance.post(`/mypage/account`),
   postBankAccount: (payload) => instance.post(`/api/mypage/account`),
   login: (payload) => instance.post(`/api/user/login`, payload),
   signup: (payload) => instance.post(`/api/user`, payload),
